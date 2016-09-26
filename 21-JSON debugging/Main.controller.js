467b4d1a37fc5e8f675d49ac0130a79226f9aa89
Wesley Aspinall <wra427@gmail.com>
2016-09-25 22:07:52 -0400

    used <pre> and |json to display json. Useful for debugging

---
+++ 21-JSON debugging/Main.controller.js
@@ -0,0 +1,16 @@
+let MainCtrl = function() {
+  let vm = this;
+  
+
+  vm.person = {
+    name: 'Wes Aspinall',
+    location: 'Miami, FL'
+  }
+
+};
+
+MainCtrl.$inject = [];
+
+angular
+  .module('app')
+  .controller('MainCtrl', MainCtrl);
