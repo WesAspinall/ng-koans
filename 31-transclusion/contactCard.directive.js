8bc5364d6cf0695236e7dd5e4f84b3085b4b5d24
Wesley Aspinall <wra427@gmail.com>
2016-09-28 04:53:49 -0400

    maunal transclusion with linking function

--- 31-transclusion/contactCard.directive.js
+++ 31-transclusion/contactCard.directive.js
@@ -4,14 +4,35 @@ function contactCard() {
     transclude: true,
     template: `
       <div>
-        <h1>contact</h1>
-        <div ng-transclude></div>
+        <span></span>
+        <div></div>
       </div>
-    `
+    `,
+    link: function($scope, $element, $attrs, $ctrl, $transclude) {
+      var div = $element.find('div');
+      var span = $element.find('span');
+      var cloned = $transclude(function(elements) {
+        elements[1].textContent = elements[1].textContent.toUpperCase();
+      });
+
+      console.log(cloned);
+      span.append(cloned[1]);
+      div.append(cloned[3]);
+    }
   }
 };
 
 angular
   .module('app')
   .directive('contactCard', contactCard);
+
+//automated single-slot transclusion with <div ng-transclude></div>
+
+// ***manual transclusion with link:
+// relies on accessing text nodes to inject
+// content.  THis is problematic when we rely on the [index] when
+// the code is
+// minified because it gets rid of whitespace and therefore messes
+// w the index
+// 
 
